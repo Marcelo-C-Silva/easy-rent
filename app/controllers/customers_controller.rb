ee7c903dac7f282aa84ba05c83fe1387c6ff9f0a
marcelo <carneiromarcelodev@gmail.com>
2024-12-20 09:49:02 -0300

    Corrigindo controlador de clientes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,36 @@
 class CustomersController < ApplicationController
+  before_action :load_customer, only: [:show, :edit, :update, :destroy]
+  def index
+    @customers = Customer.order(:name)   
+  end
+
+  def show
+
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new customer_params
+  end
+
+  def edit
+  
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def load_customer
+    @customers = Customer.find params[:id]
+  end
 end
